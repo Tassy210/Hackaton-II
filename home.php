0e7f11291f9aef2a0dc1b02e31b6f6ba72e48208
Tassy210 <43296064+Tassy210@users.noreply.github.com>
2019-11-29 19:57:31 -0300

    Esqueleto da página de login

---
+++ home.php
@@ -0,0 +1,29 @@
+<html>
+<meta charset="utf-8">
+
+<head>
+    <title>Bakof</title>
+
+</head>
+
+<body>
+    <center>
+    <h4>Bem vindo ao nosso sistema de logística da Bakof Tec</h4>
+    <br>
+
+
+        <form>
+            
+            <label>Login: </label>
+            <input type="text" placeholder="Informe seus dados">
+            <br><br>
+            <label>Senha: </label>
+            <input type="password" placeholder="Informe seus dados">
+
+        </form>
+    </center>
+</body>
+
+
+
+</html>
