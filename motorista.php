bb951df3ff1b5c1d784829cba523d5d56ca45e08
Tassy210 <43296064+Tassy210@users.noreply.github.com>
2019-11-29 20:21:35 -0300

    Formulário do motorista (Provisório)

---
+++ motorista.php
@@ -0,0 +1,43 @@
+<html>
+<meta charset="utf-8">
+
+<head>
+    <title>Cadastro motorista</title>
+
+</head>
+
+<body>
+    <center>
+    <h4>Preencha o formulário abaixo: </h4>
+    <br>
+
+
+        <form>
+            
+            <label>Nome: </label>
+            <input type="text" placeholder="Informe seus dados">
+            <br><br>
+            <label>CNPJ: </label>
+            <input type="text" placeholder="(Números somente)">
+            <br><br>
+            <label>CNPJ: </label>
+            <input type="text" placeholder="(Números somente)">
+            <br><br>
+            <label>Placa do veículo: </label>
+            <input type="text" placeholder="(Números somente)">
+            <br><br>
+            <label>Conta bancária: </label>
+            <input type="text" placeholder="Informe seus dados">
+            <br><br>
+            <label>Senha: </label>
+            <input type="password" placeholder="Informe seus dados">
+            <br><br>
+            <button type="submit" name="botao">Enviar</button>
+
+        </form>
+    </center>
+</body>
+
+
+
+</html>
