c4145b9b725c76ecbf8d2140169cd36835dbbc86
SKULL-BLACK <43687051+SKULL-BLACK@users.noreply.github.com>
2019-11-30 13:17:09 -0300

    Add files via upload

--- login_funcionario.php
+++ login_funcionario.php
@@ -4,7 +4,8 @@
 <!------ Include the above in your HEAD tag ---------->
 
 <html>
-    <meta charset="utf-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale= 1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
@@ -19,16 +20,25 @@
 			<div class="user_card">
 				<div class="d-flex justify-content-center">
 					<div class="brand_logo_container">
-						<img src="https://cdn.freebiesupply.com/logos/large/2x/pinterest-circle-logo-png-transparent.png" class="brand_logo" alt="Logo">
+						<img src="lu.png"  class="brand_logo" alt="Logo">
 					</div>
 				</div>
+
+
+
+			
+
+
 				<div class="d-flex justify-content-center form_container">
+					
+					
 					<form action ="inserts/logando2.php" method="POST">
 						<div class="input-group mb-3">
+							
 							<div class="input-group-append">
 								<span class="input-group-text"><i class="fas fa-user"></i></span>
 							</div>
-							<input type="text" name="login" class="form-control input_user" value="" placeholder="login">
+							<input type="text" name="login" class="form-control input_user" value="" placeholder="Nome do funcionario">
 						</div>
 						<div class="input-group mb-2">
 							<div class="input-group-append">
@@ -36,11 +46,7 @@
 							</div>
 							<input type="password" name="senha" class="form-control input_pass" value="" placeholder="senha">
 						</div>
-                        <div class="mt-4">
-                            <div class="d-flex justify-content-center links">
-                                Efetue o login como funcionário
-                            </div>
-                        </div>
+                      
 							<div class="d-flex justify-content-center mt-3 login_container">
 				 	<button type="submit" name="button" class="btn login_btn">Login</button>
 				   </div>
--- login_motorista.php
+++ login_motorista.php
@@ -19,7 +19,7 @@
 			<div class="user_card">
 				<div class="d-flex justify-content-center">
 					<div class="brand_logo_container">
-						<img src="https://cdn.freebiesupply.com/logos/large/2x/pinterest-circle-logo-png-transparent.png" class="brand_logo" alt="Logo">
+						<img src="lm.png" class="brand_logo" alt="Logo">
 					</div>
 				</div>
 				<div class="d-flex justify-content-center form_container">
@@ -36,11 +36,7 @@
 							</div>
 							<input type="password" name="senha" class="form-control input_pass" value="" placeholder="senha">
 						</div>
-                        <div class="mt-4">
-                            <div class="d-flex justify-content-center links">
-                                Efetue o login como motorista
-                            </div>
-                        </div>
+                      
 							<div class="d-flex justify-content-center mt-3 login_container">
 				 	<button type="submit" name="button" class="btn login_btn">Login</button>
 				   </div>
