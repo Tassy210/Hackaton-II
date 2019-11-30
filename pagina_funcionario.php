c5bf9a30bc86fc9cd89d9fe91fce126944a998ce
matheus-nicolay <58345766+matheus-nicolay@users.noreply.github.com>
2019-11-30 11:43:57 -0300

    Add files via upload

--- pagina_funcionario.php
+++ pagina_funcionario.php
@@ -1,11 +1,3 @@
-<?php
-
-session_start();
-
-echo $_SESSION['login'];
-
-?>
-
 <html>
 <meta charset="utf-8">
 
@@ -15,31 +7,56 @@ echo $_SESSION['login'];
 </head>
 
 <body>
-    <center>
-        <h2>Fala, fiote (Funcionário). Bem vindo ao nosso sistema. Show, né? Nos dá um suporte e nos ajuda à ganhar essa joça</h2>
-        <br>
-        <br>
-        <a href="motorista.php">Fazer cadastro de motorista</a><br>
-        <a href="inserts/cadastroCarga.php">Fazer cadastro de Carga</a>
-        <br>
-        <a href="selects/listarEnvios.php">Listar envios</a>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <a href="inserts/logouting.php">Logout</a>
-        <br>
-        <br><br>
-        <h4>Desenvolvido por: Matheus Nicolay, Leon Tassinari e Bruno Dutra (A.K.A Equipe 5)</h4>
-    </center>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<html>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
+
+    <head>
+        <title>Bakof</title>
+    </head>
+    <body>
+	<div class="container h-100">
+		<div class="d-flex justify-content-center h-100">
+			<div class="user_card">
+				<div class="d-flex justify-content-center">
+					<div class="brand_logo_container">
+						<img src="https://cdn.freebiesupply.com/logos/large/2x/pinterest-circle-logo-png-transparent.png" class="brand_logo" alt="Logo">
+					</div>
+				</div>
+				<div class="d-flex justify-content-center form_container">
+					<form>
+                    <div class="input-group mb-3">
+                        <a href='motorista.php' name="button" class="btn login_btn">Cadastrar motorista</a>
+                    </div>
+                    <div class="input-group mb-3">    
+						<a href='inserts/cadastroCarga.php' name="button" class="btn login_btn">Cadastrar carga</a>
+                    </div>
+                    <div class="input-group mb-3">    
+                        <a href='selects/listarEnvios.php' name="button" class="btn login_btn">Listar envios</a>
+                    </div>
+                        <div class="mt-4">
+                            <div class="d-flex justify-content-center links">
+                                Encerre a sessão
+                            </div>
+                        </div>
+							<div class="d-flex justify-content-center mt-3 login_container">
+				 	<a href='inserts/logouting.php' name="button" class="btn login_btn">Logout</a>
+				   </div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
-
-
-
+</html>
+</div>
+</body>
 </html>
--- pagina_motorista.php
+++ pagina_motorista.php
@@ -1,15 +1,63 @@
+<?php 
+
+session_start(); 
+include "conectar.php";
+
+$sql = "select * from motorista where id =".$_SESSION['id'];
+$query= mysqli_query($conexao, $sql);
+$user = mysqli_fetch_array($query);
+?>
+
 <html>
-<meta charset="utf-8">
-
 <head>
-    <title>Perfil motorista</title>
+<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
+  <title>Perfil</title>
+
+  <!-- Custom fonts for this template-->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <style type="text/css">
+      .profile-pic img {
+        width: 170px;
+        height: 170px;
+        border-radius: 50%;
+        border: 3px solid #fff;
+        margin: 0 auto;
+      }
+
+      .btn-group{
+        float: right !important;
+      }
+
+
+  </style>
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
 </head>
-
 <body>
     <center>
-    <h4>Bem vindo, (Nome do motorista)! </h4>
+    <h4>Bem vindo motorista <?= $user['nome'] ?></h4>
     <br>
+    <!-------------->
+        <!-- DataTables Example -->
+        <div class="card mb-3">
+          <div class="card-header">
+            Entregas a serem feitas
+          </div>
+          <div class="card-body">
     <?php
         require "conectar.php";
         include "confere_m.php";
@@ -18,29 +66,103 @@
         $result = mysqli_query($conexao, $select) or die ("Erro ao selecionar");
 
         while($row = mysqli_fetch_array($result)){
-            echo '<tr>';
-            echo    '<td>'; echo $row['nomeProduto']; echo '</td>';
-            echo   ' <td>'; echo $row['localSaida']; echo '</td>';
-            echo    '<td>'; echo $row['destino']; echo '</td>';
-            echo    '<td>'; echo $row['numeroNota']; echo '</td>';
-            echo '<td><a href="inserts/cadastroCargaM.php?id='.$row['idCarga'].'">Cadastrar entrega</a></td>';
-            echo  '</tr><br>';
+            echo '<div class="jumbotron text-center hoverable p-4">';
+
+                  echo '<div class="row">
+                        <div class="col-md-4 offset-md-1 mx-3 my-3">';
+
+              
+                  echo  '<br><div class="view overlay">
+                              <img src="fotos/'.$row['foto'].'" class="img-fluid" alt="Sample image for first version of blog listing">
+                              <a>
+                                <div class="mask rgba-white-slight"></div>
+                              </a>
+                              </div>
+                            </div>';
+
+                  echo '<div class="col-md-7 text-md-left ml-3 mt-3">
+
+
+                              <h2 class="h4 mb-4">'.$row['nomeProduto'].'</h2>
+
+                              <p class="font-weight-normal">'.$row['nomeProduto'].'</p>
+                              <p class="font-weight-normal">'.$row['localSaida'].'</p>
+                              <p class="font-weight-normal">'.$row['destino'].'</p>
+                              <p class="font-weight-normal">'.$row['numeroNota'].'</p>
+                              
+                              <br>
+
+                              <form method="GET" action="inserts/cadastroCargaM.php">
+                              <input type="hidden" name="id" value='.$row['idCarga'].'>
+                              <a class="botao"><input type="submit" class="btn btn-primary" value="Cadastrar entrega"></a>
+                              </form>
+                              <br><br>';
+
+
+
+                            echo '</div>
+                            <!-- Grid column -->
+
+                          </div>
+                          <!-- Grid row -->
+
+                        </div>
+                        <!-- News jumbotron -->
+                        </div>';
+    
+                echo '</div>';
         }
     ?>
-    <label>Placa do veículo: ABC-1234 (Placa fictícia DÃÃÃÃ)</label>
-
-    <table>
-    <h3>Histórico de entregas: </h3>
-    <b><label>Data da entrega: </label></b>
-    <br><br>
-    <b><label>Entrega mais recente: </label></b>
-    <br><br>
-    <b><label>Entrega mais antiga (Precisar tirar eu tiro depois, já era):</label></b>
-    </table>
-    <a href="inserts/logouting.php">Logout</a>
+    </div>
+    </div>
+    <a href="inserts/logouting.php" class="btn login_btn">Logout</a>
     </center>
+ <!-- Bootstrap core JavaScript-->
+ <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Page level plugin JavaScript-->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+  <script src="vendor/datatables/jquery.dataTables.js"></script>
+  <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin.min.js"></script>
+  <!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
+  <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/select2/select2.min.js"></script>
+  <script type="text/javascript">
+    $(".selection-1").select2({
+      minimumResultsForSearch: 20,
+      dropdownParent: $('#dropDownSelect1')
+    });
+  </script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/slick/slick.min.js"></script>
+  <script type="text/javascript" src="js/slick-custom.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/lightbox2/js/lightbox.min.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/sweetalert/sweetalert.min.js"></script>
+
+
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/parallax100/parallax100.js"></script>
+  <script type="text/javascript">
+        $('.parallax100').parallax100();
+  </script>
+<!--===============================================================================================-->
+  <script src="js/main.js"></script>
 </body>
-
-
-
 </html>
